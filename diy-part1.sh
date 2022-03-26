2cd3b4c059c2996e609f0bae43eea050b5d7fdce
aja0010101 <61653782+aja0010101@users.noreply.github.com>
2022-03-26 08:57:01 -0400

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,4 +17,4 @@
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
 echo 'src-git n2n https://github.com/alecthw/openwrt-n2n.git' >>feeds.conf.default
-echo 'src-git helloworld https://github.com/fw876/helloworld.git' >>feeds.conf.default
+# echo 'src-git helloworld https://github.com/fw876/helloworld.git' >>feeds.conf.default
