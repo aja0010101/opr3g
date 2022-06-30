e344085669e542cc39589479fb02579ed58e1c6c
aja0010101 <61653782+aja0010101@users.noreply.github.com>
2022-06-30 21:10:17 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,10 +11,17 @@
 #
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.66.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.123.1/g' package/base-files/files/bin/config_generate
+rm -rf target/linux/ath79/image/tiny-tp-link.mk
+rm -rf target/linux/ath79/dts/ar9331_tplink_tl-wr703n_tl-mr10u.dtsi
+wget https://am.sohaha.xyz/tp703/tiny-tp-link.mk -O target/linux/ath79/image/tiny-tp-link.mk
+wget https://am.sohaha.xyz/tp703/ar9331_tplink_tl-wr703n_tl-mr10u.dtsi -O target/linux/ath79/dts/ar9331_tplink_tl-wr703n_tl-mr10u.dtsi
 
-svn co https://github.com/immortalwrt/luci/branches/openwrt-21.02/applications/luci-app-ngrokc package/luci-app-ngrokc
-svn co https://github.com/immortalwrt/packages/branches/openwrt-21.02/net/ngrokc package/ngrokc
+mkdir -p package/helloworld
+for i in "dns2socks" "microsocks" "ipt2socks" "pdnsd-alt" "redsocks2"; do \
+  svn checkout "https://github.com/immortalwrt/packages/trunk/net/$i" "package/helloworld/$i"; \
+done
+
 find package/*/ -maxdepth 2 -path "*/Makefile" | xargs -i sed -i 's/include\ \.\.\/\.\.\/luci\.mk/include \$(TOPDIR)\/feeds\/luci\/luci\.mk/g' {}
 
 ./scripts/feeds update -a
