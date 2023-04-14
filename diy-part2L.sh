f18e97a8d5d241107ce38e89cdbe19455bf380f7
aja0010101 <61653782+aja0010101@users.noreply.github.com>
2023-04-14 19:59:39 +0800

    Update diy-part2L.sh

--- diy-part2L.sh
+++ diy-part2L.sh
@@ -12,6 +12,7 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.100.1/g' package/base-files/files/bin/config_generate
+mkdir -p files/etc/hotplug.d/block && curl -fsSL https://raw.githubusercontent.com/281677160/openwrt-package/usb/block/10-mount > files/etc/hotplug.d/block/10-mount
 # rm -rf target/linux/ath79/image/tiny-tp-link.mk
 # rm -rf target/linux/ath79/dts/ar9331_tplink_tl-wr703n_tl-mr10u.dtsi
 # wget https://am.sohaha.xyz/tp703/tiny-tp-link.mk -O target/linux/ath79/image/tiny-tp-link.mk
